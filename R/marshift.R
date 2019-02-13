de7fd97c235c43a47482c2714b74d43d3bb359b2
JORGE GARCIA MOLINOS <garciamj@tcd.ie>
2019-02-13 17:56:44 +0900

    marshift data set

---
+++ R/marshift.R
@@ -0,0 +1,26 @@
+#' Global metadataset of reported marine species range shifts
+#'
+#' @docType data
+#'
+#' @usage data(marshift)
+#'
+#' @keywords datasets
+#'
+#' @format An extract from the meta data set provided used by Poloczanska
+#' et al.(2013) containing species range shifts in marine taxa reported
+#' in the literature. This data set corresponds to the most updated version
+#' as provided in Brown et al.(2016). Following the original analysis, null
+#' responses have been removed and shits reported as absolute shift distances
+#' (see Poloczanska et al. 2013 for details).
+#'
+#' @source (\href{https://onlinelibrary.wiley.com/action/downloadSupplement?doi=10.1111%2Fgcb.13184&file=gcb13184-sup-0001-TableS1.csv}{Brown et al. 2016}).
+#'
+#' @references Brown et al. 2016. Ecological and methodological drivers of
+#' species distribution and phenology responses to climate change.
+#' Global Change Biol., 22, 1548-1560.
+#' (\href{https://onlinelibrary.wiley.com/doi/full/10.1111/gcb.13184}{PubMed})
+#'
+#' Poloczanska et al. 2013. Global imprint of climate change on marine life.
+#' Nat. Clim. Change, 3, 919-925.
+#' (\href{https://www.nature.com/articles/nclimate1958}{PubMed})
+"marshift"
