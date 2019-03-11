6c53cbb26f4251e1a5a0a8832411db2502f8563e
Jorge Garcia Molinos <garciamj@tcd.ie>
2019-03-11 13:02:52 +0900

    redundant call to data() deleted

--- R/tempTrend.R
+++ R/tempTrend.R
@@ -19,7 +19,6 @@
 #' @author Jorge Garcia Molinos and Christopher J. Brown
 #' @examples
 #'
-#' data(HSST)
 #' yrSST <- sumSeries(HSST, p = "1969-01/2009-12", yr0 = "1955-01-01", l = nlayers(HSST),
 #' fun = function(x) colMeans(x, na.rm = TRUE), freqin = "months", freqout = "years")
 #'
