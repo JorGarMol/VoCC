9019fe3b51e912f49b5cda978238b403f2289a41
Jorge Garcia Molinos <jorgegmolinos@arc.hokudai.ac.jp>
2022-08-15 13:33:06 +0900

    Update trajClas.R

--- R/trajClas.R
+++ R/trajClas.R
@@ -49,7 +49,7 @@
 #' @examples
 #'
 #' # input raster layers
-#' yrSST <- sumSeries(HSST, p = "1969-01/2009-12", yr0 = "1955-01-01", l = nlayers(HSST),
+#' yrSST <- sumSeries(HSST, p = "1960-01/2009-12", yr0 = "1955-01-01", l = nlayers(HSST),
 #' fun = function(x) colMeans(x, na.rm = TRUE), freqin = "months", freqout = "years")
 #'
 #' mn <- raster::calc(yrSST, mean, na.rm=T)
